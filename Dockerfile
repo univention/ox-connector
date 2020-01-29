54c343e23e74e2c51ee8cf76edb3e4ec523b2715
Dirk Wiesenthal <wiesenthal@univention.de>
2020-01-29 17:05:49 +0100

    Bug #50754: Test scripts in Dockerfile, first tests for contexts

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,13 @@ FROM python:3.8-alpine
 RUN apk add libxml2-dev gcc libc-dev libxslt-dev
 RUN pip install zeep
 
+ADD python/*.py /usr/local/lib/python3.8/site-packages/oxconnector/
+
 # for me. FIXME: remove
 RUN apk add vim
-
-ADD python/*.py /usr/local/lib/python3.8/site-packages/oxconnector/
+RUN pip install pytest uritemplate
+ADD test/*.py /usr/local/share/oxconnector/tests/
+# super simple test that the very basics work
 RUN python -c "import oxconnector.backend"
+
 CMD [ "/sbin/init" ]
---
+++ test/context.py
@@ -0,0 +1,142 @@
+#!/usr/bin/py.test
+
+import time
+
+from oxconnector import get_ox_integration_class
+from udm_rest import UDM
+
+import pytest
+
+def wait_for_listener():
+	time.sleep(10)
+
+@pytest.fixture
+def new_context_id():
+	return '23'
+
+@pytest.fixture
+def ldap_base():
+	return 'dc=intranet,dc=ox,dc=de'
+
+@pytest.fixture
+def udm_uri():
+	# cannot verify https in the container at the moment
+	return 'http://master150.intranet.ox.de/univention/udm/'
+
+@pytest.fixture
+def udm_admin_username():
+	return 'Administrator'
+
+@pytest.fixture
+def udm_admin_password():
+	return 'univention'
+
+class UDMTest(object):
+	def __init__(self, uri, ldap_base, username, password):
+		self.client = UDM.http(uri, username, password)
+		self.ldap_base = ldap_base
+		self.new_objs = {}
+
+	def create(self, module, position, attrs):
+		print('Adding {} object in {}'.format(module, position))
+		mod = self.client.get(module)
+		obj = mod.new(position='{},{}'.format(position, self.ldap_base))
+		obj.properties.update(attrs)
+		obj.save()
+		dn = obj.dn
+		print('Successfully added {}'.format(dn))
+		dns = self.new_objs.get(module, [])
+		dns.append(dn)
+		self.new_objs[module] = dns
+		return dn
+
+	def modify(self, module, dn, attrs):
+		print('Modifying {} object {}'.format(module, dn))
+		obj = self.client.get(module).get(dn)
+		obj.properties.update(attrs)
+		obj.save()
+		new_dn = obj.dn
+		print('Successfully modified {}'.format(dn))
+		if new_dn != dn:
+			dns = self.new_objs.get(module, [])
+			try:
+				dns.remove(dn)
+			except ValueError:
+				pass
+			dns.append(new_dn)
+			self.new_objs[module] = dns
+		return new_dn
+
+	def remove(self, module, dn):
+		print('Removing {} from {}'.format(dn, module))
+		obj = self.client.get(module).get(dn)
+		obj.delete()
+		dns = self.new_objs.get(module, [])
+		try:
+			dns.remove(dn)
+		except ValueError:
+			pass
+		return dn
+
+@pytest.fixture
+def udm(udm_uri, ldap_base, udm_admin_username, udm_admin_password):
+	_udm = UDMTest(udm_uri, ldap_base, udm_admin_username, udm_admin_password)
+	yield _udm
+	if _udm.new_objs:
+		print('Test done. Now removing newly added DNs...')
+		for module, dns in _udm.new_objs.items():
+			for dn in dns:
+				_udm.remove(module, dn)
+		wait_for_listener()
+
+@pytest.fixture
+def ox_host():
+	return 'master150.intranet.ox.de'
+
+def test_add_context(new_context_id, udm, ox_host):
+	udm.create('oxmail/oxcontext', 'cn=open-xchange', {
+		'hostname': ox_host,
+		'oxQuota': 1000,
+		'oxDBServer': ox_host,
+		'oxintegrationversion': '11.0.0-32A~4.4.0.201911191756',
+		'contextid': new_context_id,
+		'name': 'context{}'.format(new_context_id),
+	})
+	wait_for_listener()
+	Context = get_ox_integration_class('SOAP', 'Context')
+	c = Context.list(pattern=new_context_id)
+	assert c[0].name == 'context{}'.format(new_context_id)
+	assert c[0].max_quota == 1000
+
+def test_modify_context(new_context_id, udm, ox_host):
+	dn = udm.create('oxmail/oxcontext', 'cn=open-xchange', {
+		'hostname': ox_host,
+		'oxQuota': 1000,
+		'oxDBServer': ox_host,
+		'oxintegrationversion': '11.0.0-32A~4.4.0.201911191756',
+		'contextid': new_context_id,
+		'name': 'context{}'.format(new_context_id),
+	})
+	wait_for_listener()
+	udm.modify('oxmail/oxcontext', dn, {'oxQuota': 2000})
+	wait_for_listener()
+	Context = get_ox_integration_class('SOAP', 'Context')
+	c = Context.list(pattern=new_context_id)
+	assert c[0].name == 'context{}'.format(new_context_id)
+	assert c[0].max_quota == 2000
+
+def test_remove_context(new_context_id, udm, ox_host):
+	dn = udm.create('oxmail/oxcontext', 'cn=open-xchange', {
+		'hostname': ox_host,
+		'oxQuota': 1000,
+		'oxDBServer': ox_host,
+		'oxintegrationversion': '11.0.0-32A~4.4.0.201911191756',
+		'contextid': new_context_id,
+		'name': 'context{}'.format(new_context_id),
+	})
+	wait_for_listener()
+	udm.remove('oxmail/oxcontext', dn)
+	wait_for_listener()
+	Context = get_ox_integration_class('SOAP', 'Context')
+	c = Context.list(pattern=new_context_id)
+	assert c == []
