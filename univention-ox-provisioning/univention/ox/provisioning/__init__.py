c0e86ac570292cef024aa8ef4120fd1a51fee8d8
Dirk Wiesenthal <wiesenthal@univention.de>
2021-12-22 00:32:34 +0100

    [#381]: Provisioning of Functional Accounts

--- univention-ox-provisioning/univention/ox/provisioning/__init__.py
+++ univention-ox-provisioning/univention/ox/provisioning/__init__.py
@@ -50,6 +50,11 @@ from univention.ox.provisioning.resources import (
     delete_resource,
     modify_resource,
 )
+from univention.ox.provisioning.functional_account import (
+    create_functional_account,
+    delete_functional_account,
+    modify_functional_account,
+)
 from univention.ox.provisioning.users import create_user, delete_user, modify_user
 from univention.ox.soap.config import NoContextAdminPassword
 
@@ -96,6 +101,14 @@ def run(obj):  # noqa: C901
                 modify_resource(obj)
             elif obj.was_deleted():
                 delete_resource(obj)
+        if obj.object_type == "oxmail/functional_account":
+            for new_obj in get_account_objs(obj):
+                if new_obj.was_added():
+                    create_functional_account(new_obj)
+                elif new_obj.was_modified():
+                    modify_functional_account(new_obj)
+                elif new_obj.was_deleted():
+                    delete_functional_account(new_obj)
     except Skip as exc:
         logger.warning(f"Skipping: {exc}")
     except NoContextAdminPassword as exc:
@@ -138,6 +151,49 @@ def get_group_objs(obj):  # noqa: C901
             continue
         if context not in contexts:
             contexts.append(context)
+    for context in contexts:
+        new_obj = deepcopy(obj)
+        if new_obj.old_attributes:
+            new_obj.old_attributes["oxContext"] = context
+        if new_obj.attributes:
+            new_obj.attributes["oxContext"] = context
+        logger.info(f"{obj} will be processed with context {context}")
+        yield new_obj
+
+
+def get_account_objs(obj):  # noqa: C901
+    users = []
+    groups = []
+    ignoring = True
+    if obj.old_attributes:
+        users.extend(obj.old_attributes.get("users"))
+        groups.extend(obj.old_attributes.get("groups"))
+    if obj.attributes:
+        users.extend(obj.attributes.get("users"))
+        groups.extend(obj.attributes.get("groups"))
+    contexts = set()
+    for user in users:
+        user_obj = univention.ox.provisioning.helpers.get_old_obj(user)
+        if user_obj is None:
+            logger.info(
+                f"Account wants {user} as user. But the user is unknown. Ignoring..."
+            )
+            continue
+        try:
+            context = get_context_id(user_obj.attributes)
+        except Skip:
+            continue
+        contexts.add(context)
+    for group in groups:
+        group_obj = univention.ox.provisioning.helpers.get_old_obj(group)
+        if group_obj is None:
+            logger.info(
+                f"Account wants {group} as group. But the group is unknown. Ignoring..."
+            )
+            continue
+        for new_obj in get_group_objs(group_obj):
+            contexts.add(new_obj.attributes.get("oxContext"))
+        contexts.add(context)
     for context in contexts:
         new_obj = deepcopy(obj)
         if new_obj.old_attributes:
---
+++ univention-ox-provisioning/univention/ox/provisioning/functional_account.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2020 Univention GmbH
+#
+# http://www.univention.de/
+#
+# All rights reserved.
+#
+# The source code of this program is made available
+# under the terms of the GNU Affero General Public License version 3
+# (GNU AGPL V3) as published by the Free Software Foundation.
+#
+# Binary versions of this program provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention and not subject to the GNU AGPL V3.
+#
+# In the case you use this program under the terms of the GNU AGPL V3,
+# the program is provided in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License with the Debian GNU/Linux or Univention distribution in file
+# /usr/share/common-licenses/AGPL-3; if not, see
+# <http://www.gnu.org/licenses/>.
+
+
+import logging
+from copy import deepcopy
+
+from univention.ox.backend_base import get_ox_integration_class
+from univention.ox.provisioning.helpers import get_context_id, get_obj_by_name_from_ox, get_db_id
+
+FunctionalAccount = get_ox_integration_class("SOAP", "Account")
+logger = logging.getLogger("listener")
+
+
+def functional_account_from_attributes(attributes):
+    context_id = get_context_id(attributes)
+    functional_account = FunctionalAccount(context_id=context_id)
+    update_functional_account(functional_account, attributes)
+    return functional_account
+
+
+def update_functional_account(functional_account, attributes):
+    functional_account.name = attributes.get("name")
+    functional_account.personal = attributes.get("personal")
+    functional_account.email = attributes.get("mailPrimaryAddress")
+
+
+def update_functional_account_members(functional_account, attributes):
+    functional_account.users = [db_id for dn in attributes.get("users") if (db_id := get_db_id(dn))]
+    functional_account.groups = [db_id for dn in attributes.get("groups") if (db_id := get_db_id(dn))]
+
+
+def get_functional_account_id(attributes):
+    context_id = get_context_id(attributes)
+    name = attributes.get("name")
+    functional_account = get_obj_by_name_from_ox(FunctionalAccount, context_id, name)
+    if functional_account:
+        return functional_account.id
+
+
+def create_functional_account(obj):
+    logger.info(f"Creating {obj}")
+    logger.info("Start off by removing the old one (maybe remnants?)")
+    if obj.old_attributes is None:
+        obj.old_attributes = deepcopy(obj.attributes)
+    delete_functional_account(deepcopy(obj))
+    functional_account = functional_account_from_attributes(obj.attributes)
+    update_functional_account_members(functional_account, obj.attributes)
+    logger.info(f"Got {functional_account!r}")
+    functional_account.create()
+
+
+def modify_functional_account(obj):
+    logger.info(f"Modifying {obj}")
+    logger.info("Modify is like creating a new one...")
+    create_functional_account(obj)
+
+
+def delete_functional_account(obj):
+    logger.info(f"Deleting {obj}")
+    functional_account = functional_account_from_attributes(obj.old_attributes)
+    functional_account.remove()
+    obj.attributes = None  # make obj.was_deleted() return True
