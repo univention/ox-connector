6d379811fdb00aa5a2747ab553f5dd5614b7e3a7
Per Macha <macha@univention.de>
2023-06-19 00:00:00 +0200

    feat(standalone): add stubs
    Issue: univention/customers/dataport/team-souvap#91

---
+++ standalone-files/admember.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Stub to replace `univention.lib.admember`"""
+
+
+def is_localhost_in_admember_mode():
+    pass
+
+
+# [EOF]
---
+++ standalone-files/umc_module.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Stub to replace `univention.lib.umc_module`"""
+
+
+def get_mime_description():
+    pass
+
+
+def get_mime_type():
+    pass
+
+
+def image_mime_type_of_buffer():
+    pass
+
+
+# [EOF]
