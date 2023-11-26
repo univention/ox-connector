f1738f5b7f513f22a800d8baeb8d33475c070313
Pavlos Giannakis <pavlos@codingnerd.de>
2023-11-26 21:34:34 +0100

    test: edge cases and sad path tests

--- tests/test_resource.py
+++ tests/test_resource.py
@@ -1,21 +1,27 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # SPDX-FileCopyrightText: 2023 Univention GmbH
 
+import pytest
 from univention.ox.soap.backend_base import get_ox_integration_class
 
 
-def create_obj(udm, name, domainname, context_id, user):
+def create_obj(udm, name, domainname, context_id, user, attrs=None):
+    default_attrs = {
+        "name": name,
+        "displayname": name,
+        "description": "A description for {}".format(name),
+        "resourceMailAddress": "{}@{}".format(name, domainname),
+        "resourceadmin": str(user.properties["uidNumber"]),
+        "oxContext": context_id,
+    }
+
+    if attrs is not None:
+        default_attrs.update(attrs)
+
     dn = udm.create(
         "oxresources/oxresources",
         "cn=oxresources,cn=open-xchange",
-        {
-            "name": name,
-            "displayname": name,
-            "description": "A description for {}".format(name),
-            "resourceMailAddress": "{}@{}".format(name, domainname),
-            "resourceadmin": str(user.properties["uidNumber"]),
-            "oxContext": context_id,
-        },
+        default_attrs,
     )
     return dn
 
@@ -33,13 +39,13 @@ def find_obj(context_id, name, assert_empty=False):
 
 
 def test_add_resource_in_default_context(
-    default_ox_context,
-    new_resource_name,
-    create_ox_user,
-    udm,
-    domainname,
-    new_user_name,
-    wait_for_listener,
+        default_ox_context,
+        new_resource_name,
+        create_ox_user,
+        udm,
+        domainname,
+        new_user_name,
+        wait_for_listener,
 ):
     """
     Creating a resource without a context should create it in OX' default context
@@ -54,14 +60,14 @@ def test_add_resource_in_default_context(
 
 
 def test_add_resource(
-    new_context_id,
-    new_resource_name,
-    udm,
-    create_ox_context,
-    create_ox_user,
-    domainname,
-    new_user_name,
-    wait_for_listener,
+        new_context_id,
+        new_resource_name,
+        udm,
+        create_ox_context,
+        create_ox_user,
+        domainname,
+        new_user_name,
+        wait_for_listener,
 ):
     """
     Creating a resource should create it in OX
@@ -77,14 +83,14 @@ def test_add_resource(
 
 
 def test_modify_resource(
-    new_context_id,
-    new_resource_name,
-    udm,
-    create_ox_context,
-    create_ox_user,
-    domainname,
-    new_user_name,
-    wait_for_listener,
+        new_context_id,
+        new_resource_name,
+        udm,
+        create_ox_context,
+        create_ox_user,
+        domainname,
+        new_user_name,
+        wait_for_listener,
 ):
     """
     Modification of attributes are reflected in OX
@@ -107,14 +113,14 @@ def test_modify_resource(
 
 
 def test_remove_resource(
-    new_context_id,
-    new_resource_name,
-    udm,
-    create_ox_context,
-    create_ox_user,
-    domainname,
-    new_user_name,
-    wait_for_listener,
+        new_context_id,
+        new_resource_name,
+        udm,
+        create_ox_context,
+        create_ox_user,
+        domainname,
+        new_user_name,
+        wait_for_listener,
 ):
     """
     Deleting a resource should delete it from OX
@@ -128,14 +134,14 @@ def test_remove_resource(
 
 
 def test_change_context_resource(
-    new_context_id_generator,
-    new_resource_name,
-    udm,
-    create_ox_context,
-    create_ox_user,
-    domainname,
-    new_user_name,
-    wait_for_listener,
+        new_context_id_generator,
+        new_resource_name,
+        udm,
+        create_ox_context,
+        create_ox_user,
+        domainname,
+        new_user_name,
+        wait_for_listener,
 ):
     """
     Special case: If a resource changes its context, the object is
@@ -159,3 +165,158 @@ def test_change_context_resource(
     obj = find_obj(new_context_id2, new_resource_name)
     assert obj.display_name == "New Object in new Context"
     assert obj.description == "Soon in a new context"
+
+
+def test_empty_name_resource(
+        udm,
+        domainname,
+        default_ox_context,
+        new_user_name,
+        wait_for_listener,
+        create_ox_user
+):
+    user = create_ox_user(new_user_name)
+    empty_name = ""
+
+    # Name is required, so this should fail
+    with pytest.raises(Exception):
+        dn = create_obj(udm, empty_name, domainname, default_ox_context, user)
+        wait_for_listener(dn)
+
+
+def test_all_empty_attributes_resource(
+        default_ox_context,
+        new_resource_name,
+        create_ox_user,
+        udm,
+        domainname,
+        new_user_name,
+        wait_for_listener,
+):
+    empty_attrs = {
+        "name": "",
+        "displayname": "",
+        "description": "",
+        "resourceMailAddress": "",
+        "resourceadmin": "",
+        "oxContext": "",
+    }
+    user = create_ox_user(new_user_name)
+
+    # All fields are required (except description), so this should fail
+    with pytest.raises(Exception):
+        dn = create_obj(udm, empty_attrs["name"], domainname, default_ox_context, user)
+        wait_for_listener(dn)
+
+
+def test_unset_all_attributes_resource(
+        default_ox_context,
+        new_resource_name,
+        udm,
+        create_ox_context,
+        create_ox_user,
+        domainname,
+        new_user_name,
+        wait_for_listener,
+):
+    # Create a resource with all attributes set
+    resource_name = "TestResourceWithAttributes"
+    initial_attrs = {
+        "name": resource_name,
+        "displayname": "Test Resource",
+        "description": "A test resource",
+        "resourceMailAddress": f"{resource_name}@{domainname}",
+        "resourceadmin": "admin123",
+    }
+
+    user = create_ox_user(new_user_name)
+    dn = create_obj(udm, resource_name, domainname, default_ox_context,
+                    user, attrs=initial_attrs)
+    wait_for_listener(dn)
+    obj = find_obj(default_ox_context, resource_name)
+
+    assert obj is not None
+
+    # Unset all attributes (only description can be set to an empty value,
+    # as the other attributes are required)
+    unset_attrs = {
+        "description": "",
+    }
+
+    udm.modify("oxresources/oxresources", dn, unset_attrs)
+    wait_for_listener(dn)
+    obj_after_unset = find_obj(default_ox_context, resource_name)
+
+    assert obj_after_unset.description is None
+    assert obj_after_unset.name == resource_name
+
+
+def test_invalid_context_id_resource(
+        udm,
+        domainname,
+        wait_for_listener,
+        create_ox_user
+):
+    invalid_context_id = "invalid_context_id"
+    user = create_ox_user("test_user")
+
+    # Context ID must be an integer, so this should fail
+    with pytest.raises(Exception):
+        create_obj(udm, "invalid_resource", domainname, invalid_context_id, user)
+
+
+def test_missing_user_resource(
+        udm,
+        domainname,
+        default_ox_context,
+        wait_for_listener,
+):
+    # User is required, so this should fail
+    with pytest.raises(Exception):
+        create_obj(udm, "some_name", domainname, default_ox_context, None)
+
+
+def test_missing_name_resource(
+        udm,
+        domainname,
+        default_ox_context,
+        wait_for_listener,
+        create_ox_user
+):
+    user = create_ox_user("test_user")
+
+    # Name is required, so this should fail
+    with pytest.raises(Exception):
+        create_obj(udm, None, domainname, default_ox_context, user)
+
+
+def test_wrong_name_resource(
+        udm,
+        domainname,
+        default_ox_context,
+        wait_for_listener,
+        create_ox_user
+):
+    user = create_ox_user("test_user")
+
+    # Name must be a string, so this should fail
+    with pytest.raises(Exception):
+        create_obj(udm, [1, 2, 3], domainname, default_ox_context, user)
+
+
+def test_delete_already_deleted_resource(
+        udm,
+        domainname,
+        default_ox_context,
+        wait_for_listener,
+        create_ox_user
+):
+    user = create_ox_user("test_user")
+    dn = create_obj(udm, "resource_to_be_deleted", domainname,
+                    default_ox_context, user)
+    udm.remove("oxresources/oxresources", dn)
+    wait_for_listener(dn)
+
+    # Deleting a resource that has already been deleted should fail
+    with pytest.raises(Exception):
+        udm.remove("oxresources/oxresources", dn)
