38382ba4a3498754a78e223d2c7baa465598204a
Andreas Niemann <a.niemann@olsitec.de>
2023-12-08 11:48:24 +0100

    fix(ci): reference common-ci v1.11.x to push sbom and signature to souvap

--- docs/conf.py
+++ docs/conf.py
@@ -44,8 +44,8 @@ def read_version_from_ci() -> str:
     with open("../.gitlab-ci.yml", "r") as f:
         ci = yaml.safe_load(f)
         return ci.get(
-            "variables", {"DOC_TARGET_VERSION": "2.2.8"},
-        ).get("DOC_TARGET_VERSION")
+            "variables", {"APPCENTER_VERSION": "2.2.8"},
+        ).get("APPCENTER_VERSION")
 
 
 release = read_version_from_ci()
