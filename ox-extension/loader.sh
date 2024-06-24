ad0633a1bba0ff1bb73de44df2ba18f9d9619f52
Juan Pedro Torres <juan.torres-munoz@univention.de>
2024-06-24 21:27:34 +0200

    fix: unify loader script to the rest of containers plugin loaders

--- ox-extension/loader.sh
+++ ox-extension/loader.sh
@@ -2,18 +2,11 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 # SPDX-FileCopyrightText: 2024 Univention GmbH
 
-set -eu
-
 echo "Copying the plugins into the target"
-for source in /plugins/*
-do
-    plugin_type=$(basename "${source}")
-    target="/target/${plugin_type}"
-    if [ -d "${target}" ]
-    then
-        echo "COPY - Plugin type ${plugin_type} in /target, copying files."
-        cp -av "${source}" /target
-    else
-        echo "SKIP - Plugin type ${plugin_type} no in /target, skipping."
-    fi
-done
+cp -av /plugins/udm-hooks.d /target/admin-hooks.d
+cp -av /plugins/udm-syntax.d /target/admin-syntax.d
+cp -av /plugins/udm-handlers /target/admin-handlers
+cp -av /plugins/umc-icons /target/umc-icons
+
+cp -av /plugins/umc-modules/ /target/umc-modules
+cp -av /plugins/ldap-schema/ /target/ldap-schema
