7fffd101a1f2ee9600968e18c5126e887816a228
Felix Botner <botner@univention.de>
2024-11-06 09:49:35 +0100

    Revert "get_group_name() → fallback to name if GROUP_IDENTIFIER value is None"
    This reverts commit f26c04b6e350f4f2feb590ebf1739d141185b6b6

--- univention-ox-provisioning/univention/ox/provisioning/groups.py
+++ univention-ox-provisioning/univention/ox/provisioning/groups.py
@@ -56,8 +56,7 @@ def get_group_name(group):
     if GROUP_IDENTIFIER == "entryUUID":
         return group.entry_uuid
     else:
-        # fall back to group's name if GROUP_IDENTIFIER is None
-        return group.attributes.get(GROUP_IDENTIFIER) or group.attributes.get('name')
+        return group.attributes.get(GROUP_IDENTIFIER)
 
 
 def update_group(group, attributes, group_name):
