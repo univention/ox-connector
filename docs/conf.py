7bb75575321d5a1665e4a350bd170a3be2247dfd
Nico Gulden <gulden@univention.de>
2025-04-22 10:29:21 +0200

    feat(docs): Add navigation header and footer to docs
    The docs missed the global header and footer that embeds the
    documentation into the Univention Product Documentation navigation.
    
    A configuration setting was missing.
    
    Issue #124

--- docs/conf.py
+++ docs/conf.py
@@ -105,6 +105,7 @@ html_theme_options = {
     "typesense_document": doc_base,
     "typesense_document_version": release,
     "univention_matomo_tracking": True,
+    "univention_docs_deployment": True,
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
