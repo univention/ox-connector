e96a15eb16bb12042ec7adfe9daac8486b41f261
Johannes Königer <koeniger@univention.de>
2025-05-06 15:21:35 +0200

    fix: Move context admin check to prevent change
    With commit aa072e0d2608a51d4e82c74848ae53f9c38d897a
    changes to OX context admins in the OX database
    were prevented for some time.
    
    When the code for the OX DB cache was added,
    this guard could be circumvented when the DB
    already contained the `oxDbId`.
    
    Bug #51517
    Issue univention/open-xchange/provisioning#123

--- univention-ox-provisioning/univention/ox/provisioning/users.py
+++ univention-ox-provisioning/univention/ox/provisioning/users.py
@@ -284,24 +284,25 @@ def set_user_rights(user, obj):
 
 
 def get_user_id(attributes, lookup_ox=True):
-    if attributes.get("oxDbId"):
-        return attributes["oxDbId"]
-    if not lookup_ox:
-        return
     context_id = get_context_id(attributes)
     username = attributes.get("username")
     if username == get_context_admin_user(context_id):
         raise Skip(
             f"Not touching {username} in context {context_id}: Is context admin!",
         )
+    if attributes.get("oxDbId"):
+        return attributes["oxDbId"]
+    if not lookup_ox:
+        return None
     logger.info(f"Searching for {username} in context {context_id}")
     if not User.service(context_id).exists(
         User.service(context_id).Type(id=None, name=username)
     ):
-        return
+        return None
     user = get_obj_by_name_from_ox(User, context_id, username)
     if user:
         return user.id
+    return None
 
 
 def create_user(obj, user_copy_service=None, user_id=None):
